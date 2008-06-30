7eb344660b98a48a9aec97a5f8c0a6cba34e5fb0
Nicolas Wack <wackou@gmail.com>
2008-06-30 03:17:51 +0200

    added skeleton for media plugin

---
+++ src/smewtgui/mediaplugin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+class MediaPlugin:
+    def __init__():
+        self.mediaType = None
+        self.filenameHeuristics = None
+
+    def isValidPlugin():
+        # also validate the type of the objects
+        return self.mediaType is not None and self.filenameHeuristics is not None
