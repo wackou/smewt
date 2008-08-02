59b2ba94a37cc8d9a08a3ecf6203fdb3cba4548c
Nicolas Wack <wackou@gmail.com>
2008-08-02 22:03:49 +0200

    updated licenses using the new script

--- src/smewtgui/gui/querywidget.py
+++ src/smewtgui/gui/querywidget.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 #
 # Smewt - A smart collection manager
-# Copyright (c) 2008 Nicolas Wack
-# Copyright (c) 2008 Ricard Marxer
+# Copyright (c) 2008 Ricard Marxer <email@ricardmarxer.com>
+# Copyright (c) 2008 Nicolas Wack <wackou@gmail.com>
 #
 # Smewt is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- src/smewtgui/smewt/config.py
+++ src/smewtgui/smewt/config.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Smewt - A smart collection manager
-# Copyright (c) 2008 Nicolas Wack
+# Copyright (c) 2008 Ricard Marxer <email@ricardmarxer.com>
 #
 # Smewt is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
