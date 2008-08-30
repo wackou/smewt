19e8b36fb05f8a604b5849f38476e4801114aa47
Nicolas Wack <wackou@gmail.com>
2008-08-30 03:41:41 +0200

    some fixes to the solvers

--- src/smewtgui/smewt/solvers/mergesolver.py
+++ src/smewtgui/smewt/solvers/mergesolver.py
@@ -22,10 +22,9 @@
 from smewt.solvers.solver import Solver
 import copy
 
-
-class NaiveSolver(Solver):
+class MergeSolver(Solver):
     def __init__(self):
-        super(NaiveSolver, self).__init__()
+        super(MergeSolver, self).__init__()
 
     def solve(self, query):
         self.checkValid(query)
@@ -33,24 +32,8 @@ class NaiveSolver(Solver):
         results = sorted(query.metadata, cmp = lambda x, y: x.confidence > y.confidence)
         result = copy.copy(results[0])
 
-        #resultMediaObject = copy.copy(mediaObjects[0])
-
         for md in results[1:]:
-            merge = True
             for k, v in md.properties.items():
-                #print 'Solver: Checking property ''%s'' ::: ''%s'' (%r) -- ''%s'' (%r)' % (k, v, mediaObject.confidence[k], resultMediaObject[k], resultMediaObject.confidence[k])
-                #if mediaObject.confidence.get(k, 0.0) > resultMediaObject.confidence.get(k, 0.0):
-                #if md.confidence[k] > resultMediaObject.confidence[k]:
-                #    resultMediaObject[k] = v
-                #    resultMediaObject.confidence[k] = mediaObject.confidence[k]
-                if result[k] and v and result[k] != v:
-                    merge = False
-            # FIXME: big hack
-            if list(result.getUniqueKey()).count(None) > 0 and md.confidence < 0.6:
-                merge = False
-            if merge:
-                for k in md.properties:
-                    result[k] = md[k]
+                result[k] = v
 
         self.found(query, result)
-
