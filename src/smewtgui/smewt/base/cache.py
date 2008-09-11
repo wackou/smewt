5c7f6b0352758f1318233857b44f1ca0d6de8170
Nicolas Wack <wackou@gmail.com>
2008-09-11 02:18:32 +0200

    cache can now load and save itself in a file

--- src/smewtgui/smewt/base/cache.py
+++ src/smewtgui/smewt/base/cache.py
@@ -18,7 +18,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import cPickle
+
 globalCache = {}
+
+def load(filename):
+    try:
+        globalCache = cPickle.load(open(filename, 'rb'))
+    except IOError:
+        pass
+
+def save(filename):
+    cPickle.dump(globalCache, open(filename, 'wb'))
+
 
 def cachedmethod(function):
 
@@ -26,7 +38,8 @@ def cachedmethod(function):
         # removed the first element of args for the key, which is the instance pointer
         # we don't want the cache to know which instance called it, it is shared among all
         # instances of the same class
-        key = (function, args[1:])
+        fkey = str(args[0].__class__), function.__name__
+        key = (fkey, args[1:])
         if key in globalCache:
             return globalCache[key]
 
