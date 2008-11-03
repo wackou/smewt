b101f6c281ee1f0f2ad4a39ee4c0859953aa2a33
Nicolas Wack <wackou@gmail.com>
2008-11-03 01:00:00 +0100

    moved levenshtein distance to utils module

--- src/smewtgui/smewt/utils.py
+++ src/smewtgui/smewt/utils.py
@@ -66,6 +66,37 @@ def multipleMatchRegexp(string, regexp):
 
 
 
+def levenshtein(a, b):
+    if not a: return len(b)
+    if not b: return len(a)
+
+    m = len(a)
+    n = len(b)
+    d = []
+    for i in range(m+1):
+        d.append([0] * (n+1))
+
+    for i in range(m+1):
+        d[i][0] = i
+
+    for j in range(n+1):
+        d[0][j] = j
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if a[i-1] == b[j-1]:
+                cost = 0
+            else:
+                cost = 1
+
+            d[i][j] = min(d[i-1][j] + 1,     # deletion
+                          d[i][j-1] + 1,     # insertion
+                          d[i-1][j-1] + cost # substitution
+                          )
+
+    return d[m][n]
+
+
 # filename-related functions
 import os.path,  fnmatch
 
