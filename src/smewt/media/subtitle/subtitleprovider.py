fbf223650e6d8bb47c9e9450d116dfbb6958ca57
Nicolas Wack <wackou@gmail.com>
2009-05-21 01:13:24 +0200

    much better (and more smewt-like) API for subtitle provider.
    Actually, done this way there is no need anymore to distinguish between
    movie subtitle provider and series subtitle provider from an API point of view.

---
+++ src/smewt/media/subtitle/subtitleprovider.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Smewt - A smart collection manager
+# Copyright (c) 2008 Nicolas Wack <wackou@gmail.com>
+#
+# Smewt is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Smewt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+'''
+NOTES:
+
+ - the API should be aware that there might be different releases for the same file (eg: multiple
+   release groups). It should return as much info as possible so that we might try to match this
+   info with the one we have from the movie itself
+
+ - the API should be aware that some subtitles might span more than 1 file (eg: multiple CDs movies)
+
+ - if the source of the subtitles allows downloading all of the subs for a season at once, this should
+   be cached in the implementation, but should not appear in the API.
+
+TODO: what about the encoding? utf-8 sounds better, but IIRC mplayer prefers iso-8859-1...
+'''
+
+class SubtitleNotFoundError(SmewtException):
+    pass
+
+
+
+class SubtitleProvider:
+    '''This class represents the interface that needs to be implemented by a plugin which downloads
+    subtitles for movies or series.'''
+
+
+    def getAvailableSubtitles(metadata):
+        '''This method should return a graph of all available Subtitle metadata objects for
+        the given Metadata (Movie, Episode) object.
+
+        If no subtitle could be found, this method should return an empty graph.
+
+        It is recommended to check the type of the given Metadata object upon entering this method.
+
+        NB: This graph can contain multiple versions of the subtitle (different languages,
+        different rips, ...) for the same object.
+        The subtitles may also span multiple files (if the object (movie) is split in the same way).'''
+
+        raise NotImplementedError
+
+
+    def getSubtitle(subtitle):
+        '''This method should return the contents of the given subtitle as a string.
+        The Subtitle object should be picked among the ones returned by the getAvailableSubtitles
+        method.
+
+        If the subtitle could not be found, a SubtitleNotFoundError exception should be raised.'''
+
+        raise NotImplementedError
