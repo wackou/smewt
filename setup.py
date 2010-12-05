8d82a28e658b7c0d61dcf53c0a58e4a80bc1dc07
Nicolas Wack <wackou@gmail.com>
2010-12-05 15:18:21 +0100

    some work on setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,44 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 import os
 
-media_imports = []
-for root, dirs, files in os.walk('smewt/media'):
-    media_imports += [ os.path.join(root, '*.' + ext)[6:] for ext in ('png', 'tmpl', 'css', 'html', 'js') ]
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-icons_imports = [ 'icons/*.' + ext for ext in ('png', 'svg') ]
+version = '0.2b1'
 
-print '*'*100
-print find_packages()
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'pygoo',
+    'pycurl',
+    'cheetah',
+    'lxml',
+    'feedparser'
+]
 
-setup(
-    name = 'Smewt',
-    version = '0.2',
-    author = 'Nicolas Wack',
-    author_email = 'wackou@gmail.com',
-    packages = find_packages(),
-    #package_data = { 'smewt': media_imports + icons_imports },
-    package_data = dict((package, [ '*.png', '*.svg', '*.tmpl', '*.css', '*.html', '*.js' ]) for package in find_packages()),
-    scripts = [ 'bin/smewg' ],
-    url = 'http://www.smewt.com/',
-    license = 'LICENSE.txt',
-    description = 'Smewt - a smart media manager.',
-    long_description = open('README.txt').read(),
+## media_imports = []
+## for root, dirs, files in os.walk('smewt/media'):
+##     media_imports += [ os.path.join(root, '*.' + ext)[6:] for ext in ('png', 'tmpl', 'css', 'html', 'js') ]
 
-    install_requires = [ 'pygoo', 'pycurl', 'cheetah', 'lxml', 'feedparser' ]
-)
+## icons_imports = [ 'icons/*.' + ext for ext in ('png', 'svg') ]
+
+datafiles_exts = [ '*.png', '*.svg', '*.tmpl', '*.css', '*.html', '*.js' ]
+
+setup(name = 'smewt',
+      version = version,
+      description = 'Smewt - a smart media manager.',
+      long_description = README + '\n\n' + NEWS,
+      classifiers = [], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords = 'pygoo object graph mapper',
+      author = 'Nicolas Wack',
+      author_email = 'wackou@gmail.com',
+      url = 'http://www.smewt.com/',
+      license = 'GPLv3',
+      packages = find_packages(exclude = [ 'ez_setup', 'examples', 'tests' ]),
+      #package_data = dict((package, datafiles_exts) for package in find_packages()),
+      include_package_data = True,
+      scripts = [ 'bin/smewg' ],
+      install_requires = install_requires
+      )
