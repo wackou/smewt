bb176838eeb2effff035784bebdb8a7a9ca06807
Nicolas Wack <wackou@gmail.com>
2010-12-26 00:05:50 +0100

    reset_Config.sh now also works on mac

--- bin/reset_config.sh
+++ bin/reset_config.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 rm -fr $HOME/.config/DigitalGaia/*
-rm /tmp/smewt.cache
+rm -fr $HOME/Library/Preferences/Smewt*collection*
+rm -fr $HOME/Library/Preferences/com.smewt.*
+rm -fr /tmp/smewt.cache
 
 # also remove all *.pyc files
-find -iname "*.pyc" -exec rm {} \;
+find . -iname "*.pyc" -exec rm {} \;
