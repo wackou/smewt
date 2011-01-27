15197c68050ca4476238d62b6563aa648ad0b6d6
Nicolas Wack <wackou@gmail.com>
2011-01-27 18:24:26 +0100

    Tagged 0.2 release

--- smewt/__init__.py
+++ smewt/__init__.py
@@ -19,7 +19,7 @@
 #
 
 
-__version__ = '0.2-dev'
+__version__ = '0.2'
 
 import logging
 
@@ -39,4 +39,4 @@ log = logging.getLogger('smewt')
 # used to be able to store settings for different versions of Smewt installed on the same computer, ie: a stable
 # and a development version
 ORG_NAME = 'Falafelton'
-APP_NAME = 'Smewt-dev'
+APP_NAME = 'Smewt'
